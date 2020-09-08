c52e41a17394e1db9ee009c0cd82ccb189a238f1
hslee <hslee@flyhigh-x.com>
2020-09-08 17:35:24 +0900

    practice * toggle tab * ripple button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,92 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
-import { Application } from "@nativescript/core";
+import { Application, EventData, LayoutBase, View, TouchGestureEventData, Label } from "@nativescript/core";
+import { CubicBezierAnimationCurve } from "@nativescript/core/ui/animation";
 
 @Component({
     selector: "Home",
-    templateUrl: "./home.component.html"
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+    @ViewChild('layout', { static: true }) layout: ElementRef;
+    @ViewChild('button', { static: true }) button: ElementRef;
 
     constructor() {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
-        // Init your component properties here.
+        console.log("load");
+
+        // let lb = args.object as LayoutBase;
+        let lb = this.layout.nativeElement as LayoutBase;
+        let rootView = lb.parentNode.getViewById("selected") as View;
+
+        lb.eachChildView((view: View) => {
+            if (view.className === "selected") {
+                return;
+            }
+            view.on("tap", (a: EventData) => {
+                let label = a.object as View;
+                let loc = label.getLocationRelativeTo(lb);
+                // console.log(loc);
+
+                rootView.animate({
+                    translate: { x: loc.x - 8, y: 0 },
+                    duration: 350,
+                    curve: new CubicBezierAnimationCurve(0.6, 0.72, 0, 1)
+                });
+            });
+            return true;
+        });
+    }
+
+    onClickTab(args: EventData) {
+        console.log("tab!")
+    }
+    onTouchTab(args: TouchGestureEventData) {
+        console.log("touch!");
+        if (args.action !== "down") {
+            return;
+        }else{
+            let x = args.getX();
+            let y = args.getY();
+    
+            let btn = args.object as View;
+            let wrapper = btn.parentNode as LayoutBase;
+            console.log(wrapper.typeName);
+            console.log(wrapper.className);
+    
+            let circle = new Label();
+    
+            let d = Math.max(Number(btn.height), Number(btn.width));
+    
+            circle.width = d;
+            circle.height = d;
+            circle.borderRadius = d / 2;
+            circle.top = y - d / 2;
+            circle.left = x - d / 2;
+            circle.backgroundColor = 'white';
+            circle.opacity = 0;
+    
+            wrapper.addChild(circle);
+    
+            circle.animate({
+                scale:{x:0, y:0},
+                opacity: 0.4,
+                duration: 1
+            }).then(()=>{
+                circle.animate({
+                    scale:{x:2, y:2},
+                    opacity: 0,
+                    duration: 500
+                }).then(()=>{
+                    wrapper.removeChild(circle);
+                });
+            });
+        }
+
     }
 
     onDrawerButtonTap(): void {
