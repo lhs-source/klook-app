1dedb381b0fab1be461f1b8e87b1bebd1ae5035f
hslee <hslee@flyhigh-x.com>
2020-09-29 23:44:28 +0900

    메인 화면 어느정도 틀을 갖춤

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { RouterExtensions } from "@nativescript/angular";
 
 import { AnimationCurve } from "@nativescript/core/ui/enums";
+import { LayoutBase } from "@nativescript/core/ui";
+import {screen} from "tns-core-modules/platform/platform"
 
 @Component({
     selector: "home",
@@ -9,6 +11,8 @@ import { AnimationCurve } from "@nativescript/core/ui/enums";
     styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+    @ViewChild('octopus', { static: true }) octopus: ElementRef;
+    @ViewChild('rootlayout', { static: true }) rootlayout: ElementRef;
     constructor(private routerExtensions: RouterExtensions) {
         console.log("constructor HomeComponent");
     }
@@ -16,6 +20,27 @@ export class HomeComponent implements OnInit {
     ngOnInit(): void {
         console.log("ngOnInit HomeComponent");
         console.log(this.routerExtensions.router.url);
+    }
+
+    ngAfterViewInit(){
+
+        let rlb = this.rootlayout.nativeElement as LayoutBase;
+        console.log(rlb.effectiveHeight);
+        let lb = this.octopus.nativeElement as LayoutBase;
+        console.log(lb.translateY);
+        console.log(screen.mainScreen.heightDIPs);
+        console.log(screen.mainScreen.heightPixels);
+        lb.translateY = screen.mainScreen.heightDIPs - 144;
+    }
+
+    onLoadOctopus(event){
+        let rlb = this.rootlayout.nativeElement as LayoutBase;
+        console.log(rlb.effectiveHeight);
+        console.log(rlb.getMeasuredHeight());
+        console.log(rlb.height);
+        let lb = this.octopus.nativeElement as LayoutBase;
+        console.log(lb.translateY);
+        // lb.translateY = rlb.effectiveHeight - 96;
     }
 
     navigateOnlinepay(event) {
@@ -50,7 +75,7 @@ export class HomeComponent implements OnInit {
 
     }
 
-    
+
     navigateQrScan(event) {
         console.log("navigateQrScan HomeComponent");
         this.routerExtensions.navigate(['/main/home/qr-scan'], { transition: { name: 'fade', duration: 350, curve: AnimationCurve.easeOut } });
