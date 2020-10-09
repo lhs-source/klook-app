dac766aa71c4e95192cf53b03b060fdfb62f59df
hslee <hslee@flyhigh-x.com>
2020-10-09 17:24:21 +0900

    옥토퍼스 충전 화면 수정 * 키보드 만들어야 한다...

--- src/app/main/octopus/octopus-charge/octopus-charge.component.ts
+++ src/app/main/octopus/octopus-charge/octopus-charge.component.ts
@@ -2,9 +2,10 @@ import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 
 import { AnimationCurve } from "@nativescript/core/ui/enums";
 import { RouterExtensions } from "@nativescript/angular";
-import { LayoutBase, ScrollEventData, ScrollView, Image, Label, ViewBase } from "tns-core-modules";
+import { LayoutBase, ScrollEventData, ScrollView, Image, Label, ViewBase, GridLayout, GridUnitType } from "tns-core-modules";
 import { screen } from "tns-core-modules/platform/platform"
 import { ActivatedRoute } from "@angular/router";
+import { ItemSpec } from "tns-core-modules/ui/layouts/grid-layout";
 
 @Component({
     selector: "octopus-charge",
@@ -13,6 +14,9 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class OctopusChargeComponent implements OnInit {
     tag = this.constructor.name;
+
+    @ViewChild('cardframe', {static:true}) cardframe : ElementRef;
+    @ViewChild('cardelems', {static:true}) cardelems : ElementRef;
 
     card_index = 0;
 
@@ -33,7 +37,28 @@ export class OctopusChargeComponent implements OnInit {
             this.card_index = params['index'];
             console.log("index = " + this.card_index);
         }));
+        
+        
+    }
+    onLoadedCardImage(event){
+        let p = screen.mainScreen.heightDIPs / screen.mainScreen.heightPixels;
+        setTimeout(() => {
+            let img = this.cardframe.nativeElement as Image;
+            let lb = this.cardelems.nativeElement as GridLayout;
 
+            let img_height = img.getMeasuredHeight() * p;
+            let top_bottom_height = 60;
+            let shadow_margin = 46;
+
+
+            console.log(img.getMeasuredHeight() * p);
+            console.log(lb);
+            let parent = img.parentNode;
+            lb.removeRows();
+            lb.addRow(new ItemSpec(top_bottom_height, GridUnitType.PIXEL));
+            lb.addRow(new ItemSpec((img_height - top_bottom_height * 2 - shadow_margin), GridUnitType.PIXEL));
+            lb.addRow(new ItemSpec(top_bottom_height, GridUnitType.PIXEL));            
+        });
     }
     
     ngAfterViewInit():void{
