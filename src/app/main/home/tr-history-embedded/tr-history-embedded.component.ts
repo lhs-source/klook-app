5b1f068017bdef84d9fa1ddb1eaf2ca8238ae81e
hslee <hslee@flyhigh-x.com>
2020-10-15 22:03:27 +0900

    거래내역 추가 및 데이터로부터 뷰 만들도록 수정

--- src/app/main/home/tr-history-embedded/tr-history-embedded.component.ts
+++ src/app/main/home/tr-history-embedded/tr-history-embedded.component.ts
@@ -9,6 +9,56 @@ import { isAndroid, Application, AndroidApplication, AndroidActivityBackPressedE
 })
 export class TrHistoryEmbeddedComponent implements OnInit {
     tag = this.constructor.name;
+    // trs
+    icons = {
+        "department":"~/images/ico_type2.png",
+        "grocery":"~/images/ico_type3.png",
+        "restorant":"~/images/ico_type5.png",
+        "point":"~/images/ico_type1.png",
+        "mart":"~/images/ico_type4.png",
+        "sport":"~/images/ico_type6.png",
+        "cafe":"~/images/ico_type7.png",
+        "exchange":"~/images/ico_type8.png",
+    };
+    trs={
+        "10월 30일 금요일":[
+            {
+                icon:"department",
+                merchant:"Central Department Store",
+                point:25000,
+                curr:676,
+                date: new Date(2020, 10, 30, 14, 23, 0, 0),
+                description:"포인트사용",
+                taxfree: true,
+                utu:false,
+                save_point:1250,
+            },{
+                icon:"grocery",
+                merchant:"Family mart",
+                point:4500,
+                curr:122,
+                date: new Date(2020, 10, 30, 11, 30, 0, 0),
+                description:"포인트사용",
+                taxfree: false,
+                utu:true,
+                save_point:45,
+            }
+        ],
+        "10월 27일 화요일":[
+            {
+                icon:"point",
+                merchant:"KB국민카드 Nori카드 충전",
+                point:100000,
+                curr:0,
+                date: new Date(2020, 10, 27, 21, 10, 0, 0),
+                description:"포인트 일반충전",
+                taxfree: false,
+                utu:false,
+                save_point:0,
+            }
+        ]
+    }
+
     constructor(private routerExtensions: RouterExtensions) {
         console.log(`${this.tag} constructor `)
         
--- src/app/main/tr/tr-history/tr-history.component.ts
+++ src/app/main/tr/tr-history/tr-history.component.ts
@@ -7,6 +7,7 @@ import { screen } from "tns-core-modules/platform";
 
 import { CustomTransition, CustomTransitionBack } from "../../home/klook-transition";
 import { animate, JsAnimationDefinition } from "./animation-helpers";
+import { KeyValue } from "@angular/common";
 
 @Component({
     selector: "tr-history",
@@ -26,48 +27,53 @@ export class TrHistoryComponent implements OnInit {
 
     // trs
     icons = {
-        "department":"~/images/ico_type2.png",
-        "grocery":"~/images/ico_type3.png",
-        "restorant":"~/images/ico_type5.png",
-        "point":"~/images/ico_type1.png",
-        "mart":"~/images/ico_type4.png",
-        "sport":"~/images/ico_type6.png",
-        "cafe":"~/images/ico_type7.png",
-        "exchange":"~/images/ico_type8.png",
+        "department": "~/images/ico_type2.png",
+        "grocery": "~/images/ico_type3.png",
+        "restorant": "~/images/ico_type5.png",
+        "point": "~/images/ico_type1.png",
+        "mart": "~/images/ico_type4.png",
+        "sport": "~/images/ico_type6.png",
+        "cafe": "~/images/ico_type7.png",
+        "exchange": "~/images/ico_type8.png",
     };
-    trs = [
-        {
-            icon:"department",
-            merchant:"Central Department Store",
-            point:25000,
-            curr:676,
-            date: new Date(2020, 10, 30, 14, 23, 0, 0),
-            description:"포인트사용",
-            taxfree: true,
-            utu:false,
-            save_point:1250,
-        },{
-            icon:"grocery",
-            merchant:"Family mart",
-            point:4500,
-            curr:122,
-            date: new Date(2020, 10, 30, 11, 30, 0, 0),
-            description:"포인트사용",
-            taxfree: false,
-            utu:true,
-            save_point:45,
-        },{
-            icon:"point",
-            merchant:"KB국민카드 Nori카드 충전",
-            point:100000,
-            curr:0,
-            date: new Date(2020, 10, 27, 21, 10, 0, 0),
-            description:"포인트 일반충전",
-            taxfree: false,
-            utu:false,
-            save_point:0,
-        }
-    ]
+    trs = {
+        "10월 30일 금요일": [
+            {
+                icon: "department",
+                merchant: "Central Department Store",
+                point: 25000,
+                curr: 676,
+                date: new Date(2020, 10, 30, 14, 23, 0, 0),
+                description: "포인트사용",
+                taxfree: true,
+                utu: false,
+                save_point: 1250,
+            }, {
+                icon: "grocery",
+                merchant: "Family mart",
+                point: 4500,
+                curr: 122,
+                date: new Date(2020, 10, 30, 11, 30, 0, 0),
+                description: "포인트사용",
+                taxfree: false,
+                utu: true,
+                save_point: 45,
+            }
+        ],
+        "10월 27일 화요일": [
+            {
+                icon: "point",
+                merchant: "KB국민카드 Nori카드 충전",
+                point: 100000,
+                curr: 0,
+                date: new Date(2020, 10, 27, 21, 10, 0, 0),
+                description: "포인트 일반충전",
+                taxfree: false,
+                utu: false,
+                save_point: 0,
+            }
+        ]
+    }
 
     constructor(private routerExtensions: RouterExtensions) {
         console.log(`${this.tag} constructor `)
@@ -76,10 +82,10 @@ export class TrHistoryComponent implements OnInit {
             Application.android.off(AndroidApplication.activityBackPressedEvent);
             Application.android.on(AndroidApplication.activityBackPressedEvent, (data: AndroidActivityBackPressedEventData) => {
                 console.log("back button pressed on " + this.tag);
-                if(this.routerExtensions.router.isActive('/main/tr/history', false) === true){
+                if (this.routerExtensions.router.isActive('/main/tr/history', false) === true) {
                     data.cancel = true;
-                    this.routerExtensions.navigate(['/main/home'], { transition:{instance : new CustomTransitionBack(250, AnimationCurve.linear)}, clearHistory: true });
-                }else{
+                    this.routerExtensions.navigate(['/main/home'], { transition: { instance: new CustomTransitionBack(250, AnimationCurve.linear) }, clearHistory: true });
+                } else {
                     data.cancel = false;
                 }
             });
@@ -101,22 +107,22 @@ export class TrHistoryComponent implements OnInit {
                 return;
             }
             this.tab_view.push(view);
-            
+
             let temp_color = new Color("black");
             let origin_color = new Color("#888");
-            
+
             // first sel elems
-            if(this.tab_view.length === 1){
-                view.on('loaded', (a)=>{
+            if (this.tab_view.length === 1) {
+                view.on('loaded', (a) => {
                     console.log(view);
-                    let color_anim : JsAnimationDefinition = {
-                        curve: t=>t,
-                        getRange: () => { return {from:view.color.r, to:temp_color.r}; },
+                    let color_anim: JsAnimationDefinition = {
+                        curve: t => t,
+                        getRange: () => { return { from: view.color.r, to: temp_color.r }; },
                         step: (v) => view.color = new Color(255, v, v, v)
                     };
                     animate(250, [color_anim]);
 
-                    setTimeout(()=>{
+                    setTimeout(() => {
                         let label = a.object as View;
                         let loc = label.getLocationRelativeTo(lb);
                         let width = label.getMeasuredWidth();
@@ -140,17 +146,17 @@ export class TrHistoryComponent implements OnInit {
 
                 // v = variation
                 // step: (v) => label.color = new Color(255, v, v, v)
-                let color_anim : JsAnimationDefinition = {
-                    curve: t=>t,
-                    getRange: () => { return {from:label.color.r, to:temp_color.r}; },
+                let color_anim: JsAnimationDefinition = {
+                    curve: t => t,
+                    getRange: () => { return { from: label.color.r, to: temp_color.r }; },
                     step: (v) => label.color = new Color(255, v, v, v)
                 };
                 animate(250, [color_anim]);
                 this.tab_view.forEach((t) => {
-                    if(t !== label){
-                        let color_anim : JsAnimationDefinition = {
-                            curve: t=>t,
-                            getRange: () => { return {from:t.color.r, to:origin_color.r}; },
+                    if (t !== label) {
+                        let color_anim: JsAnimationDefinition = {
+                            curve: t => t,
+                            getRange: () => { return { from: t.color.r, to: origin_color.r }; },
                             step: (v) => t.color = new Color(255, v, v, v)
                         };
                         animate(250, [color_anim]);
@@ -158,14 +164,18 @@ export class TrHistoryComponent implements OnInit {
                 });
                 rootView.animate({
                     translate: { x: loc.x, y: 0 },
-                    width:width * p,
+                    width: width * p,
                     duration: 250,
                     curve: new CubicBezierAnimationCurve(0.6, 0.72, 0, 1)
                 });
             });
-            
+
             return true;
         });
+    }
+
+    keyDescOrder = (a: KeyValue<number, string>, b: KeyValue<number, string>): number => {
+        return a.key > b.key ? -1 : (b.key > a.key ? 1 : 0);
     }
 
     // actionbar emit click close
@@ -174,12 +184,12 @@ export class TrHistoryComponent implements OnInit {
 
         this.routerExtensions.navigate(['/main/home'], {
             clearHistory: true,
-            transition: { instance : new CustomTransitionBack(250, AnimationCurve.linear) }
+            transition: { instance: new CustomTransitionBack(250, AnimationCurve.linear) }
         });
     }
 
 
-    onTapFirst(){
+    onTapFirst() {
         // this.routerExtensions.navigate(['/main/home'], {
         //     clearHistory: true,
         //     transition: { instance : new CustomTransitionBack(250, AnimationCurve.linear) }
@@ -189,7 +199,7 @@ export class TrHistoryComponent implements OnInit {
 
     navigateDetails(event) {
         console.log(this.tag + " navigateChargePoint");
-        this.routerExtensions.navigate(['/main/tr/detail'], { transition: { instance : new CustomTransition(250, AnimationCurve.linear) } });
+        this.routerExtensions.navigate(['/main/tr/detail'], { transition: { instance: new CustomTransition(250, AnimationCurve.linear) } });
 
     }
 }
